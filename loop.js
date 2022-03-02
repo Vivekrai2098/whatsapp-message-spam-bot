f473f6b92db53f2a4dfdbaa451397e7c067cca53
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-03-02 21:33:41 +0530

    message  code

---
+++ loop.js
@@ -0,0 +1,6 @@
+var vo = document.getElementsByClassName("_13NKt copyable-text selectable-text")[1];      
+var ho = new InputEvent("input",{ bubbles: true });
+for (let i=0;i<=10;i++){
+vo.innerHTML="p";
+vo.dispatchEvent(ho);
+document.getElementsByClassName("_4sWnG")[0].click();}
